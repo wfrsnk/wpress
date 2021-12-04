2dac36cfe4f22bdfe21a03b521ab9d52d21501c8
wfrsnk <57827569+wfrsnk@users.noreply.github.com>
2021-12-04 14:48:19 +0300

    a

---
+++ index.js
@@ -0,0 +1,38 @@
+import express from 'express';
+
+const PORT = process.env.PORT || 80;
+const user = {
+    username: 'gossjsstudent2017',
+    password: '|||123|||456'
+}
+const app = express();
+
+app
+    .get("/wordpress", async(req, res, next) => {
+        const content = req.query.content;
+        const response = await fetch('https://wordpress.kodaktor.ru/wp-json/jwt-auth/v1/token', {
+            method: 'POST',
+            headers: {
+                'Content-Type':'application/json;charset=utf-8'
+            },
+            body: JSON.stringify(user)
+        });
+        const token = response.data.token;
+
+        const wpResponse = fetch('https://wordpress.kodaktor.ru/wp-json/wp/v2/posts', {
+            method: 'POST',
+            headers: {
+                Authorization: `Bearer ${token}`
+            },
+            body: JSON.stringify({content, title:"wfrsnk", status:"publish"})
+        })
+        res.send(wpResponse.data.id + ''); 
+    })
+
+
+    .listen(PORT, ()=>{
+        console.log('Server has been started...');
+    });
+
+
+    
